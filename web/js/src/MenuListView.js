76afac740e157f731853e509fa21f0cffe78c89c
itokami1123dev <kamiari819dev@gmail.com>
2013-10-19 08:49:55 +0900

    レコード追加イベント時の動きを変更

--- web/js/src/MenuListView.js
+++ web/js/src/MenuListView.js
@@ -2,7 +2,8 @@
 var MenuListView = Backbone.View.extend({
     el:'#menu-table',
     initialize: function(options) {
-        this.listenTo( this.collection, 'reset add destroy', this.listRender);
+        this.listenTo( this.collection, 'reset destroy', this.listRender);
+        this.listenTo( this.collection, 'add', this.addRender);
     },
     listRender: function(){
 		this.$el.empty();
@@ -12,7 +13,19 @@ var MenuListView = Backbone.View.extend({
         var opt = {model:menuItem};
         var menuItemView = new MenuItemView(opt);
         menuItemView.createRender();
-        this.$el.append( menuItemView.$el);
+        var $itemView = menuItemView.$el;
+        this.$el.append( $itemView);
+    },
+
+    addRender: function(menuItem){
+        var opt = {model:menuItem};
+        var menuItemView = new MenuItemView(opt);
+        menuItemView.createRender();
+        var $itemView = menuItemView.$el;
+        $itemView.css({"opacity":0.0});
+        this.$el.append( $itemView);
+        $itemView.animate({"opacity":1.0}, 500);
     }
+
 });
 
