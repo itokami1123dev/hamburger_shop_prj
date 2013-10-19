0854658b487f0e3f730850584612fd1e0e8b3712
itokami1123dev <kamiari819dev@gmail.com>
2013-10-19 13:20:38 +0900

    Enterキーによる入力補助機能

--- web/js/src/MenuNewItemView.js
+++ web/js/src/MenuNewItemView.js
@@ -5,16 +5,34 @@ var MenuNewItemView = Backbone.View.extend({
 	initialize:function( options){ 
 	},
     events: {
+        "keypress": "keypressHandler",
         "click button":"createItem"
+    },
+    keypressHandler: function(event){
+    	var KEY_ENTER = 13;
+        if ( event.keyCode== KEY_ENTER ){
+        	var $target = $(event.target);
+        	if ( $target.data("tagId")==="new-price" ){
+        		this.createItem(); 
+
+        	}else{
+				this.$('[data-tag-id="new-price"]').focus();
+        	}
+        }
     },
 	createItem: function(){
 		var $name = this.$('[data-tag-id="new-name"]');
 		var $price = this.$('[data-tag-id="new-price"]');
+
+		if ( $name.val().length === 0 ){ return }
+		if ( $price.val().length === 0 ){ return }
+		if ( !isFinite($price.val()) ){ return }
+
 		this.collection.createMenu( 
 			$name.val(), 
 			parseInt( $price.val(), 10)
 		);
-		$name.val("");
+		$name.val("").focus();
 		$price.val("");
 	}
 });
